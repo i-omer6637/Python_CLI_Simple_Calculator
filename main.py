fc3991d66c23f3488e1da6a50acd415de5a74cc1
Ryleebonbon <2burtonr@qegs.email>
2026-02-05 16:32:50 +0000

    Update main.py
    Combined all the defs into operate().
    Added a try except loop for errors.

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
-import os
-
-# A simple calculator
-
-def main():
-    while True:
-        print("Welcome to Calculator")
-        choice = input("Press \n 1 to Add \n 2 to Sub \n 3 to Multiple \n 4 to divide \n :")
-        num1 = input("Enter first no : ")
-        num2 = input("Enter second no : ")
-        match float(choice):
-            case 1:
-                add(num1, num2)
-            case 2:
-                sub(num1, num2)
-            case 3:
-                multi(num1, num2)
-            case 4:
-                divid(num1, num2)
-            case _:
-                print("Invalid Option")
-        
-        loop_check = input("Press 1 to Try Again : ")
-        os.system('cls')
-        if(int(loop_check) != 1):
-            break
-
-
+try:
+    import os
+    
+    # A simple calculator
+    print("Welcome to Calculator")
+    def main():
+        while True:
+            choice = input("Press \n 1 to Add \n 2 to Sub \n 3 to Multiple \n 4 to divide \n :")
+            num1 = input("Enter first no : ")
+            num2 = input("Enter second no : ")
+            match int(choice):
+                case 1:
+                    operate("+",num1, num2)
+                case 2:
+                    operate("-",num1, num2)
+                case 3:
+                    operate("*",num1, num2)
+                case 4:
+                    operate("/",num1, num2)
+                case _:
+                    print("Invalid Option")
             
-def add(num1,num2):
-    result = float(num1) + float(num2)
-    print("Result is " , result)
-
-
-def sub(num1,num2):
-    result = float(num1) - float(num2)
-    print("Result is " , result)
-
-def multi(num1,num2):
-    result = float(num1) * float(num2)
-    print("Result is " , result)
-
-def divid(num1,num2):
-    result = float(num1) / float(num2)
-    print("Result is " , result)
-
-
-main()
+            loop_check = input("Press 1 to Try Again : ")
+            os.system('cls')
+            if(int(loop_check) != 1):
+                break
+    
+    def operate(operator,first,second):
+        string = f"global var1; var1 = {first}{operator}{second}"
+        exec(string)
+        print(var1)
+    
+    main()
+except:
+    print("Error, try again\n")
+    main()
